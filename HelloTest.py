ba29791ddf3b953d179e481802ae3db423d465f3
samikacem <61453697+samikacem@users.noreply.github.com>
2023-12-05 13:27:53 +0100

    firstTest

---
+++ HelloTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def hello_world():
+    print("Hello, World!")
+
+def hello():
+    print("Hello!")    
+
+# Call the function to print "Hello, World!"
+hello_world()
+hello()
